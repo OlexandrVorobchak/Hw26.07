4fd9e5e2614203ec546145859a9c3d8c4b5a3cb9
OlexandrVorobchak <eclipsesaers@gmail.com>
2024-01-07 22:18:39 +0200

    1
    1

---
+++ Hw26.07/Hw26.07/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hw26._07.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
